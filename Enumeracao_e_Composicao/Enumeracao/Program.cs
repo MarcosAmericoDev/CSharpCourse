f43657406914933c39e402616ce4dbf2149782c8
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-03-05 11:36:37 -0300

    Criando repositório, adicionando pasta do capítulo 9 (enumeração e composição) e criando a pasta de enumeração

---
+++ Enumeracao_e_Composicao/Enumeracao/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enumeracao.Entities;
+using Enumeracao.Entities.Enums;
+using System.Globalization;
+
+namespace Enumeracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1000,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
