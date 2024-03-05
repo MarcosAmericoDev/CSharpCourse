0deb516d0cb991ada9e3e97b89a260d8d2c465e4
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-03-05 16:55:43 -0300

    Adicionado o segundo exercício propostos (Poste e comentários - Demonstração da classe StringBuilder)

---
+++ Enumeracao_e_Composicao/ExerciciosEnumEComp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using ExerciciosEnumEComp2.Entities;
+
+namespace ExerciciosEnumEComp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Travelling to the New Zealing",
+                "I'm going to visit this wonderful country!",
+                12);
+            p1.addComment(c1);
+            p1.addComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:09"),
+                "Good night guys",
+                "See you tomorrow",
+                5);
+
+            p2.addComment(c3);
+            p2.addComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
