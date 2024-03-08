c2d62c7964fa533a49ef063228b11fb97bc86c13
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-03-08 18:39:02 -0300

    Adicionando exercício resolido sobre Herança e Polimorfismo (Funcionários Tercerizados)

---
+++ Heranca_e_Polimorfismo/ExercicioFuncionariosTercerizados/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFuncionariosTercerizados.Entities
+{
+    internal class OutSourcedEmployee : Employee
+    {
+        public double AddicionalCharge { get; set; }
+
+        public OutSourcedEmployee() 
+        {
+        }
+
+        public OutSourcedEmployee(string name, int hours, double valuePerHour, double addicionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AddicionalCharge = addicionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AddicionalCharge;
+        }
+    }
+}
---
+++ Heranca_e_Polimorfismo/ExercicioFuncionariosTercerizados/Program.cs
@@ -0,0 +1,49 @@
+﻿using ExercicioFuncionariosTercerizados.Entities;
+using System.Globalization;
+
+namespace ExercicioFuncionariosTercerizados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> listOfEmployees = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int nEmployee = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nEmployee; i++) 
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsourced (y/n)? ");
+                Char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string tempName = Console.ReadLine();
+                Console.Write("Hours worked: ");
+                int tempHours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double tempValuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'y') 
+                {
+                    Console.Write("Addicional charge: ");
+                    double tempAddicionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    listOfEmployees.Add(new OutSourcedEmployee(tempName, tempHours, tempValuePerHour, tempAddicionalCharge));
+                } 
+                else
+                {
+                    listOfEmployees.Add(new Employee(tempName, tempHours, tempValuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+
+            foreach (Employee emp in listOfEmployees)
+            {
+                Console.WriteLine($"{emp.Name} - ${emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+
+
+        }
+    }
+}
