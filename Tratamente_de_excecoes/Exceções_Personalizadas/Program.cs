e052749cfc84bef51b788452114da7bcdab381dc
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-03-14 17:34:28 -0300

    Adicionando aula de exceções personalizadas parte 1 (estudo de caso ficticio: exemplo muito ruim)

---
+++ Tratamente_de_excecoes/Exceções_Personalizadas/Program.cs
@@ -0,0 +1,48 @@
+﻿using Exceções_Personalizadas.Entities;
+
+namespace Exceções_Personalizadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int numberRoom = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkInRoom = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOutRoom = DateTime.Parse(Console.ReadLine());
+
+            if (checkOutRoom <= checkInRoom)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            }
+            else
+            {
+                Reservation reservationRoom = new Reservation(numberRoom, checkInRoom, checkOutRoom);
+                Console.WriteLine($"Reservation: {reservationRoom}");
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkInRoom = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOutRoom = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                if (checkInRoom < now || checkOutRoom < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates");
+                } else if(checkOutRoom <= checkInRoom) {
+                    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+                } else
+                {
+                    reservationRoom.UpdateDates(checkInRoom, checkOutRoom);
+                    Console.WriteLine($"Reservation: {reservationRoom}");
+                }
+            }
+        }
+    }
+}
