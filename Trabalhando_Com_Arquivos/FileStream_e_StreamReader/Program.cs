eba0f197621876436f2be00c6b7c6263ee1b6149
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-10-18 14:56:08 -0300

    modificando para ler várias linhas

--- Trabalhando_Com_Arquivos/FileStream_e_StreamReader/Program.cs
+++ Trabalhando_Com_Arquivos/FileStream_e_StreamReader/Program.cs
@@ -10,9 +10,11 @@
             try
             {
                 sr = File.OpenText(path);
-
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
+                while(!sr.EndOfStream) 
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
             }
             catch (IOException e)
             {
