08b1ffabaae1a7bc32fbfdc58cceb67b51b17a00
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-10-18 15:03:53 -0300

    Adicionando aula do bloco using

---
+++ Trabalhando_Com_Arquivos/usingBlock/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace usingBlock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occorred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
