4ac1ffe265ffdb8a69785345a2095a748e53758b
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-10-18 15:14:27 -0300

    Adiciona a aula referente ao StreamWriter

---
+++ Trabalhando_Com_Arquivos/StreamWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StreamWriterAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occorred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
