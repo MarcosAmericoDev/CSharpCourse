37b9974837c5a0ad3edd3243b7c005d6293188ea
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-10-22 14:17:15 -0300

    Féita exercício do módulo de Trabalhando com Arquivos, finalizado o módulo

---
+++ Trabalhando_Com_Arquivos/ExercicioArquivos/Entities/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioArquivos.Entities
+{
+    class Produto
+    {
+
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int QuantityInStorage { get; private set; }
+
+        public Produto(string name, double price, int quantityInStorage)
+        {
+            Name = name;
+            Price = price;
+            QuantityInStorage = quantityInStorage;
+        }
+
+        public double TotalPrice()
+        {
+            return Price * QuantityInStorage;
+        }
+    }
+}
---
+++ Trabalhando_Com_Arquivos/ExercicioArquivos/Program.cs
@@ -0,0 +1,46 @@
+﻿using ExercicioArquivos.Entities;
+using System.Globalization;
+
+namespace ExercicioArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFilePath = @"c:\temp\source.csv";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                Console.WriteLine(targetFolderPath);
+
+                using (StreamWriter sr = File.AppendText(targetFilePath)) 
+                {
+                    foreach(string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Produto p = new Produto(name, price, quantity);
+
+                        sr.WriteLine(p.Name + "," + p.TotalPrice().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            } 
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occorred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
