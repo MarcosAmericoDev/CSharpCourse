944e11a6c8a44ac22b66178f4f676d180537f170
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-10-28 17:17:19 -0300

    Começando módulo de Generics, Set e Dictionary. Fazendo problema motivador 1.

---
+++ Interfaces/EstudoIComparable/Entites/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace EstudoIComparable.Entites
+{
+    class Employee : IComparable<Employee>
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Employee emp)
+        {
+            return Name.CompareTo(emp.Name);
+        }
+    }
+}
---
+++ Interfaces/EstudoIComparable/Program.cs
@@ -0,0 +1,36 @@
+﻿using EstudoIComparable.Entites;
+
+namespace EstudoIComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+            string outpath = @"c:\temp\out.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            try
+            {
+                using(StreamReader sr = fileInfo.OpenText())
+                {
+                    List<Employee> list = new List<Employee>();
+
+                    while(!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occorred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
