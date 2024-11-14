61ea285f4fad2bed08ed8190c1d7a908ce488604
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-11-14 14:48:22 -0300

    adicionando estudo sobre como coleções Hash testam igualdade

--- Generics_Set_Dictionary/Conjuntos/Program.cs
+++ Generics_Set_Dictionary/Conjuntos/Program.cs
@@ -1,36 +1,26 @@
-﻿namespace Conjuntos
+﻿using Conjuntos.Entities;
+
+namespace Conjuntos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
-            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+            HashSet<Product> a = new HashSet<Product>();
 
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1200.00));
+            
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
 
-            // union
-            SortedSet<int> c = new SortedSet<int>(a);
-            c.UnionWith(b);
-            PrintCollection(c);
+            Product prod = new Product("Notebook", 1200.00);
+            Console.WriteLine(a.Contains(prod));
 
-            // intersection
-            SortedSet<int> d = new SortedSet<int>(a);
-            d.IntersectWith(b);
-            PrintCollection(d);
-
-            // difference
-            SortedSet<int> e = new SortedSet<int>(a);
-            e.ExceptWith(b);
-            PrintCollection(e);
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p));
         }
 
-        static void PrintCollection<T>(IEnumerable<T> collection)
-        {
-            foreach (T obj in collection)
-            {
-                Console.Write(obj + " ");
-            }
-            Console.WriteLine();
-        }
     }
 }
