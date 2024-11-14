80a3dfba17362418eca6fbf15a86dfa991a62723
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-11-14 14:10:14 -0300

    Primeiros testes com Conjuntos (HashSet)

---
+++ Generics_Set_Dictionary/Conjuntos/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Conjuntos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Table");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
