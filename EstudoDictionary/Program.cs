9bc3e4635fb0a73e53e164793a2d8cb655a3f81c
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-11-19 17:33:11 -0300

    Adicionando estudo sobre Dictionary

---
+++ EstudoDictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace EstudoDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "997123123";
+            cookies["phone"] = "921387135"; // está sobrescrevendo o valor anterior
+
+            Console.WriteLine(cookies["phone"]);
+            Console.WriteLine(cookies["email"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email")) 
+            {
+                Console.WriteLine(cookies["email"]);
+            } else
+            {
+                Console.WriteLine("There's no 'email' key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+            Console.WriteLine("ALL COOKIES: ");
+
+            foreach (var item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
