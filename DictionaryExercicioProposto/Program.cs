dc5439e4b0acde0c04ea5105015f2dae074143bc
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-11-19 18:07:31 -0300

    Adicionando exercício proposto sobre dictionary e finalizando módulo

--- DictionaryExercicioProposto/Program.cs
+++ DictionaryExercicioProposto/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter file full path: ");
+            string source = Console.ReadLine();
+            SortedDictionary<string, int> election = new SortedDictionary<string, int>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(source))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int numberOfVotes = int.Parse(line[1]);
+                        if (election.ContainsKey(name))
+                        {
+                            election[name] += numberOfVotes;
+                            continue;
+                        }
+                        election[name] = numberOfVotes;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (KeyValuePair<string, int> pair in election)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
         }
     }
 }
