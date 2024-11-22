0ee09a37725a6d3399341621e8039c524cb3920a
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-11-22 13:24:04 -0300

    Começando módulo de tópicos especiais parte depois e colocando primeiro exemplo

---
+++ Topicos_Especiais_Parte_2/EstudoExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using EstudoExtensionMethods.Extensions;
+
+namespace EstudoExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2024, 11, 22, 11, 20, 10);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
